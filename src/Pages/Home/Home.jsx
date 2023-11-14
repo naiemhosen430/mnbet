8e820dac0a874d92dcd095a8f8c6bb525a36ed2a
naiemhosen430 <naiemhosen430@gmail.com>
2023-11-14 07:05:34 -0800

    d

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,31 @@
-import Hero from "./Components/Hero";
-
 export default function Home() {
   return (
     <>
-      <Hero />
+      <div className="p-10">
+        <div className="lg:mx-10 backdrop-blur-md border border-white rounded-2xl p-5">
+          <div className="flex justify-center items-center">
+            <div className="w-4/12">
+              <img
+                className="lg:w-40 w-20 lg:h-60 h-20 block m-auto"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRupAZSlEcehQSIxhIWiwVLwkkfokFmutoTqbX-Hmxxev2H25uTGbvM-06PacyaCmrmFHA&usqp=CAU"
+                alt=""
+              />
+            </div>
+            <div className="w-4/12">
+              <h1 className="text-xl lg:text-4xl text-slate-500 font-bold lg:text-right text-center">
+                সহজ ব্যাটিং বেশি টাকা
+              </h1>
+            </div>
+            <div className="w-4/12">
+              <img
+                className="lg:w-40 w-20 lg:h-60 h-20 block m-auto"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStE8Nxq4ula9yfuY_qQAuksgN2jPrjj5xRZbrLHZpaGxlkToP_5525JKdH9KeGHM1CllM&usqp=CAU"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
