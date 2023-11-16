368db46a1eadd92a31dd036534368b79cedde8e3
naiemhosen430 <naiemhosen430@gmail.com>
2023-11-15 19:47:54 -0800

    d

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { router } from "./Router/Router";
 import { RouterProvider } from "react-router-dom";
 import { UserAuthContextProvider } from "./Context/UseAuthContext";
-
-import { router } from "./Router/Router";
+import React from "react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <UserAuthContextProvider>
-    <RouterProvider router={router} />
-  </UserAuthContextProvider>
+  <React.StrictMode>
+    <UserAuthContextProvider>
+      <RouterProvider router={router} />
+    </UserAuthContextProvider>
+  </React.StrictMode>
 );
