ef9c89a005a2e1eca2b6b04bbd998c73e0b2b9e0
naiemhosen430 <naiemhosen430@gmail.com>
2023-11-15 19:51:02 -0800

    d

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { router } from "./Router/Router";
 import { RouterProvider } from "react-router-dom";
 import { UserAuthContextProvider } from "./Context/UseAuthContext";
-import React from "react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
